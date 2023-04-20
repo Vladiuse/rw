be8e778330ee932dde82a6cdaf5f2d1eb6ecf3a3
vladiuse <vladiuse@gmail.com>
2023-04-20 22:05:16 +0300

    add tegs file

---
+++ rw/containers/templatetags/client.py
@@ -0,0 +1,28 @@
+from django import template
+from django.contrib.auth.models import Group
+
+register = template.Library()
+
+
+@register.filter(name='left')
+def left(value, arg):
+    leng = int(arg)
+    return f'{value:_<{leng}}'
+
+
+@register.filter(name='right')
+def right(value, arg):
+    leng = int(arg)
+    return f'{value:_>{leng}}'
+
+
+@register.filter(name='center_dash')
+def center_dash(value, arg):
+    leng = int(arg)
+    return f'{value:_^{leng}}'
+
+@register.filter(name='has_group')
+def has_group(user,group):
+    if Group.objects.get(name=group) in user.groups.all():
+        return True
+    return False
