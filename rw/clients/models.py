dd441b1e7c71e556267bfa9a0e2a8886cd614bde
vladiuse <vladiuse@gmail.com>
2025-02-18 15:25:38 +0300

    add Book model

--- rw/clients/models.py
+++ rw/clients/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from .types import CALL_TO_CLIENTS_BOOK, UNLOADING_BOOK
 
-# Create your models here.
+class Book(models.Model):
+    BOOK_TYPES = (
+        (CALL_TO_CLIENTS_BOOK, CALL_TO_CLIENTS_BOOK),
+        (UNLOADING_BOOK, UNLOADING_BOOK),
+    )
+    file = models.FileField(upload_to='books')
+    created = models.DateTimeField(auto_now_add=True)
+    book_date = models.DateField(default=timezone.now())
+    description = models.CharField(max_length=255, blank=True)
+    type = models.CharField(max_length=30, choices=BOOK_TYPES)
