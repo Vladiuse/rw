4bec5c772f6bfd42af1c9051e4651a96bb7ba75f
vladiuse <vladiuse@gmail.com>
2025-02-18 15:48:20 +0300

    add func get_random_file_name

--- rw/common/utils.py
+++ rw/common/utils.py
@@ -1,3 +1,6 @@
+import random
+from string import ascii_letters
+
 def month_text(month_num):
     data = {
         '1': 'января',
@@ -14,3 +17,9 @@ def month_text(month_num):
         '12': 'декабря',
     }
     return data[str(month_num)]
+
+def get_random_file_name(length=10):
+    result = ''
+    for _ in range(length):
+        result += random.choice(ascii_letters)
+    return result
