064eba0475e5a6f8a5c7d7f50474c4002194ef2c
vladiuse <vladiuse@gmail.com>
2025-02-19 15:28:04 +0300

    add some old code

--- rw/clients/models.py
+++ rw/clients/models.py
@@ -3,6 +3,31 @@ from django.utils import timezone
 from datetime import timedelta, datetime
 from django.core.validators import MaxValueValidator
 from .types import CALL_TO_CLIENTS_BOOK, UNLOADING_BOOK
+from django.db import connection
+
+QUERY = """
+SELECT client_name, COUNT(*)as count , 
+ROUND(AVG(DATEDIFF('%s', start_date))) as past,
+CASE
+    WHEN COUNT(*) > 1 THEN MAX( DATEDIFF('%s', start_date))
+    ELSE '-'
+END as max,
+CASE
+    WHEN COUNT(*) > 1 THEN MIN( DATEDIFF('%s', start_date))
+    ELSE '-'
+END as min
+FROM clients_container
+WHERE book_id = %d
+GROUP BY client_name ORDER BY count DESC;
+"""
+
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
 
 class Book(models.Model):
     BOOK_TYPES = (
@@ -16,6 +41,13 @@ class Book(models.Model):
     type = models.CharField(max_length=30, choices=BOOK_TYPES, default=CALL_TO_CLIENTS_BOOK)
     no_containers_file = models.FileField(upload_to='books_no_containers', blank=True)
     error_text = models.TextField(blank=True)
+
+    def client_count(self):
+        with connection.cursor() as cursor:
+            result_query = QUERY % (self.book_date, self.book_date, self.book_date, self.pk)
+            cursor.execute(result_query)
+            rows = dictfetchall(cursor)
+        return rows
 
 
 class Container(models.Model):
