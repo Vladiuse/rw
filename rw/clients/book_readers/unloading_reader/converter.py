3d4c54391af02f5dcc13ce467fa707fe41d0e48f
vladiuse <vladiuse@gmail.com>
2025-02-19 12:59:16 +0300

    refactor UnloadingBookTextConverter

--- rw/clients/book_readers/unloading_reader/converter.py
+++ rw/clients/book_readers/unloading_reader/converter.py
@@ -1,34 +1,23 @@
 from datetime import date, datetime
 from .dto import UploadingContainer
-from clients.book_readers.dto import FileContainerExistLines
-from common.containers.utils import is_line_contain_container
-
 
 
 class UnloadingBookTextConverter:
 
-    def convert(self, text: str) -> FileContainerExistLines:
-        lines_with_container = []
-        lines_without_containers = []
-        lines = text.split('\n')
-        for line in lines:
-            if is_line_contain_container(line=line):
-                item = UploadingContainer(
-                    container_number=self._get_container(line=line),
-                    start_date=self._get_start_date(line=line),
-                    client_name=self._get_client_name(line=line),
-                    nn=self._get_nn(line=line),
-                    send_number=self._get_send_number(line=line),
-                    weight=self._get_weight(line=line),
-                    area=self._get_area(line=line),
-                )
-                lines_with_container.append(item)
-            else:
-                lines_without_containers.append(line)
-        return FileContainerExistLines(
-            lines_with_container=lines_with_container,
-            lines_without_containers=lines_without_containers,
-        )
+    def convert(self, lines_with_containers:list[str]) -> list[UploadingContainer]:
+        uploading_containers = []
+        for line in lines_with_containers:
+            item = UploadingContainer(
+                container_number=self._get_container(line=line),
+                start_date=self._get_start_date(line=line),
+                client_name=self._get_client_name(line=line),
+                nn=self._get_nn(line=line),
+                send_number=self._get_send_number(line=line),
+                weight=self._get_weight(line=line),
+                area=self._get_area(line=line),
+            )
+            uploading_containers.append(item)
+        return uploading_containers
 
     def _get_container(self, line: str) -> str:
         return line[44:55]
