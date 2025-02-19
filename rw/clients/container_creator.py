64123549fd08510879cc6c4e296ba944056598b6
vladiuse <vladiuse@gmail.com>
2025-02-19 13:29:02 +0300

    add ContainerCreatorFromBook

--- rw/clients/container_creator.py
+++ rw/clients/container_creator.py
@@ -3,18 +3,32 @@ from .types import CALL_TO_CLIENTS_BOOK, UNLOADING_BOOK
 from clients.book_readers.call_to_client_reader import ClientCallTextLineConverter
 from clients.book_readers.unloading_reader import UnloadingBookTextConverter
 from .utils import create_no_containers_file
+from clients.book_reader.container_separator import ContainerSeparator
+from .exception import ContainerNotFound
 
+class ContainerCreatorFromBook:
+    separator = ContainerSeparator()
 
-class CallClientContainerCreator:
-    converter = ClientCallTextLineConverter()
+    def create_containers_items(self, book: Book, lines_with_container: list[str]) -> list[Container]:
+        raise NotImplementedError
 
     def create(self, book: Book):
         text = book.file.read().decode('utf-8')
-        result = self.converter.convert(text=text)
-        lines_without_containers_text = '\n'.join(result.lines_without_containers)
-        create_no_containers_file(book=book, text=lines_without_containers_text)
+        separated_lines = self.separator.separate(lines=text.split('\n'))
+        if len(separated_lines.lines_with_container) == 0:
+            raise ContainerNotFound('В файле не найдены строки с контейнерами')
+        create_no_containers_file(book=book, text=separated_lines.lines_without_containers)
+        containers_to_create = self.create_containers_items(book=book, lines_with_container=separated_lines.lines_with_container)
+        Container.objects.bulk_create(containers_to_create)
+
+
+class CallClientContainerCreator(ContainerCreatorFromBook):
+    converter = ClientCallTextLineConverter()
+
+    def create_containers_items(self, book: Book,lines_with_container: list[str]):
+        containers_data = self.converter.convert(lines_with_containers=lines_with_container)
         containers_to_create = []
-        for container_data in result.lines_with_container:
+        for container_data in containers_data:
             container = Container(
                 document=book,
                 number=container_data.container_number,
@@ -23,21 +37,16 @@ class CallClientContainerCreator:
                 end_date=container_data.end_date,
             )
             containers_to_create.append(container)
-        result = Container.objects.bulk_create(containers_to_create)
-        print(result)
-        print(type(result))
+        return containers_to_create
 
 
 class UnloadingContainerCreator:
     converter = UnloadingBookTextConverter()
 
-    def create(self, book: Book):
-        text = book.file.read().decode('utf-8')
-        result = self.converter.convert(text=text)
-        lines_without_containers_text = '\n'.join(result.lines_without_containers)
-        create_no_containers_file(book=book, text=lines_without_containers_text)
+    def create_containers_items(self, book: Book,lines_with_container: list[str]):
+        containers_data = self.converter.convert(lines_with_containers=lines_with_container)
         containers_to_create = []
-        for container_data in result.lines_with_container:
+        for container_data in containers_data.lines_with_container:
             container = Container(
                 document=book,
                 number=container_data.container_number,
@@ -50,17 +59,13 @@ class UnloadingContainerCreator:
                 area=container_data.area,
             )
             containers_to_create.append(container)
-        result = Container.objects.bulk_create(containers_to_create)
-        print(result)
-        print(type(result))
-
+        return containers_to_create
 
 
 creators = {
     CALL_TO_CLIENTS_BOOK: CallClientContainerCreator(),
     UNLOADING_BOOK: UnloadingContainerCreator(),
 }
-
 
 def create_containers(book: Book):
     creator = creators[book.type]
