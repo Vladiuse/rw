917306f2c21645c6f584fccc1f86d795481a0c2e
vladiuse <vladiuse@gmail.com>
2026-01-14 22:19:20 +0300

    add func to models

--- rw/clients/models.py
+++ rw/clients/models.py
@@ -1,10 +1,13 @@
-from django.db import models
-from django.db.models import F, Count, Min, Max, Avg, ExpressionWrapper
-from django.db.models.query import QuerySet
-from django.db.models.fields import DurationField, DateField, DateTimeField
-from django.utils import timezone
-from datetime import timedelta, datetime, date
+from datetime import date, datetime, timedelta
+
 from django.core.validators import MaxValueValidator
+from django.db import models
+from django.db.models import Avg, Case, Count, DateField, ExpressionWrapper, F, Max, Min, Q, When
+from django.db.models.fields import DateTimeField
+from django.db.models.functions import TruncDate
+from django.db.models.query import QuerySet
+from django.utils import timezone
+
 from .types import CALL_TO_CLIENTS_BOOK, UNLOADING_BOOK
 
 
@@ -15,17 +18,17 @@ class Book(models.Model):
         (CALL_TO_CLIENTS_BOOK, CALL_TO_CLIENTS_BOOK),
         (UNLOADING_BOOK, UNLOADING_BOOK),
     )
-    file = models.FileField(upload_to='books')
+    file = models.FileField(upload_to="books")
     created = models.DateTimeField(auto_now_add=True)
     book_date = models.DateTimeField(default=timezone.now())
     description = models.CharField(max_length=255, blank=True)
     type = models.CharField(max_length=30, choices=BOOK_TYPES, default=UNLOADING_BOOK)
-    no_containers_file = models.FileField(upload_to='books_no_containers', blank=True)
+    no_containers_file = models.FileField(upload_to="books_no_containers", blank=True)
     error_text = models.TextField(blank=True)
 
 
 class Container(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='containers', related_query_name='container')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="containers", related_query_name="container")
     number = models.CharField(max_length=11)
     client_name = models.CharField(max_length=30)
     start_date = models.DateTimeField(default=None, null=True)
@@ -35,72 +38,118 @@ class Container(models.Model):
     weight = models.CharField(max_length=5, blank=True)
     area = models.PositiveIntegerField(default=None, null=True, validators=[MaxValueValidator(99)])
 
-    def time_delta_past(self):
-        """Время простоя"""
-        return str(timezone.now().date() - self.start_date).split(',')[0]
+    def time_delta_past(self) -> str:
+        """Время простоя."""
+        return str(timezone.now().date() - self.start_date).split(",")[0]
 
-    def is_past_30(self):
-        """Является ли простой больше месяца"""
-        if hasattr(self, 'past'):
-            return self.past.days >= 29
-        if self.end_date:
-            diff  = self.end_date - self.start_date
-        else:
-            diff  = datetime.now().date() - self.start_date
-        return diff.days >= 29
+    def is_past_30(self) -> bool:
+        """Является ли простой больше месяца."""
+        month_day_size = 29
+        if hasattr(self, "past"):
+            return self.past.days >= month_day_size
+        diff = self.end_date - self.start_date if self.end_date else datetime.now().date() - self.start_date
+        return diff.days >= month_day_size
+
 
 def get_col_name_by_book(book: Book) -> str:
     if book.type == CALL_TO_CLIENTS_BOOK:
-        col_name = 'Вывезено КТК'
+        col_name = "Вывезено КТК"
     elif book.type == UNLOADING_BOOK:
-        col_name = 'Наличие КТК'
+        col_name = "Наличие КТК"
     else:
-        raise ValueError('Type for book not found')
+        raise ValueError("Type for book not found")
     return col_name
 
 
 def get_end_date_by_book_type(book: Book) -> date | F:
-    """Получить стартовую дату в зависимости от типа книги"""
+    """Получить стартовую дату в зависимости от типа книги."""
     if book.type == CALL_TO_CLIENTS_BOOK:
         end_date = ExpressionWrapper(
-            F('end_date'),
+            F("end_date"),
             output_field=DateTimeField(),
         )
     elif book.type == UNLOADING_BOOK:
         end_date = book.book_date
     else:
-        raise ValueError('Type for book not found')
+        raise ValueError("Type for book not found")
     return end_date
 
+
 def get_grouped_by_client_book(book: Book) -> QuerySet[Container]:
-    """Получить данные по контейнерам с групировкой по клиентам"""
+    """Получить данные по контейнерам с группировкой по клиентам."""
     end_date = get_end_date_by_book_type(book=book)
-    qs =  (
+    qs = (
         Container.objects.filter(book=book)
-        .annotate(past=end_date - F('start_date'))
-        .values('client_name')
-        .annotate(
-            count=Count('client_name'),
-            max=Max('past'),
-            min=Min('past'),
-            average_past=Avg('past')
-        )
-        .order_by('-count', '-average_past')
+        .annotate(past=end_date - F("start_date"))
+        .values("client_name")
+        .annotate(count=Count("client_name"), max=Max("past"), min=Min("past"), average_past=Avg("past"))
+        .order_by("-count", "-average_past")
     )
     return qs
 
+
 def get_book_stat(book: Book) -> dict:
-    """Получить статистику по книге"""
+    """Получить статистику по книге."""
     end_date = get_end_date_by_book_type(book=book)
-    return Container.objects.filter(book=book).annotate(past=end_date - F('start_date')).aggregate(
-        clients_count=Count('client_name', distinct=True),
-        containers_count=Count('number'),
-        average_past=Avg('past'),
-        max=Max('past'),
-        min=Min('past'),
+    return (
+        Container.objects.filter(book=book)
+        .annotate(past=end_date - F("start_date"))
+        .aggregate(
+            clients_count=Count("client_name", distinct=True),
+            containers_count=Count("number"),
+            average_past=Avg("past"),
+            max=Max("past"),
+            min=Min("past"),
+        )
     )
 
-def get_containers_with_past(book: Book) ->  QuerySet[Container]:
-    """Получить контейнеры с временем простоя"""
+
+def get_containers_with_past(book: Book) -> QuerySet[Container]:
+    """Получить контейнеры с временем простоя."""
     end_date = get_end_date_by_book_type(book=book)
-    return Container.objects.filter(book=book).annotate(past=end_date - F('start_date')).order_by('client_name', '-past')
+    return (
+        Container.objects.filter(book=book).annotate(past=end_date - F("start_date")).order_by("client_name", "-past")
+    )
+
+
+def group_containers_by_day_night(book: Book, day_start_at: int, night_start_at: int) -> list[dict]:
+    """Группировка по дням с учетом диапазона день и ночь.
+
+    Пример:
+        [
+           {'base_day': datetime.date(2025, 1, 1), 'day_count': 3, 'night_count': 3},
+           {'base_day': datetime.date(2025, 1, 2), 'day_count': 3, 'night_count': 2}
+        ]
+    """
+    if book.type != CALL_TO_CLIENTS_BOOK:
+        msg = f"Incorrect book type. must be {CALL_TO_CLIENTS_BOOK}"
+        raise TypeError(msg)
+    if day_start_at >= night_start_at:
+        raise ValueError('"day_start_at" must be less that "night_start_at"')
+    shifted_dt = ExpressionWrapper(
+        F("end_date") - timedelta(days=1),
+        output_field=DateTimeField(),
+    )
+
+    return list(
+        Container.objects.filter(book=book)
+        .annotate(
+            base_day=Case(
+                When(end_date__hour__lt=day_start_at, then=TruncDate(shifted_dt)),
+                default=TruncDate("end_date"),
+                output_field=DateField(),
+            ),
+        )
+        .values("base_day")
+        .annotate(
+            day_count=Count(
+                "id",
+                filter=Q(end_date__hour__gte=day_start_at, end_date__hour__lt=night_start_at),
+            ),
+            night_count=Count(
+                "id",
+                filter=Q(end_date__hour__gte=night_start_at) | Q(end_date__hour__lt=day_start_at),
+            ),
+        )
+        .order_by("base_day")
+    )
