d33c04a17896e3f7f577854a449ceae2e9856249
vladiuse <vladiuse@gmail.com>
2026-01-14 21:41:16 +0300

    ruff refactor

--- rw/clients/views.py
+++ rw/clients/views.py
@@ -1,68 +1,82 @@
-from stdnum import iso6346
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, redirect
-from django.contrib import messages
 from django.db.models import Count
-from django.http import HttpResponse
-from .forms import TextBookForm
-from .models import Book, get_col_name_by_book, get_book_stat, get_grouped_by_client_book, get_containers_with_past
-from .container_creator import create_containers
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
 from django.views import View
+from stdnum import iso6346
+
 from clients.book_readers.exception import ContainerFileReadError
+
+from .container_creator import create_containers
+from .forms import TextBookForm
+from .models import Book, get_book_stat, get_col_name_by_book, get_containers_with_past, get_grouped_by_client_book
 from .types import BOOK_EXAMPLES
 
 
-def index(request):
-    return HttpResponse('Clients app')
+def index(request: HttpRequest) -> HttpResponse:
+    _ = request
+    return HttpResponse("Clients app")
+
 
 @login_required
-def book_list(request):
-    books = Book.objects.annotate(containers_count=Count('container')).order_by('-book_date', '-pk')
+def book_list(request: HttpRequest) -> HttpResponse:
+    books = Book.objects.annotate(containers_count=Count("container")).order_by("-book_date", "-pk")
     content = {
-        'books': books,
+        "books": books,
     }
-    return render(request, 'clients/book/book_list.html', content)
+    return render(request, "clients/book/book_list.html", content)
+
 
 class LoadBookFileView(LoginRequiredMixin, View):
-    template_name = 'clients/book/create_book.html'
+    template_name = "clients/book/create_book.html"
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:  # noqa: ANN002, ANN003
+        (
+            _,
+            _,
+        ) = args, kwargs
         form = TextBookForm()
         content = {
-            'form': form,
-            'book_examples': BOOK_EXAMPLES,
+            "form": form,
+            "book_examples": BOOK_EXAMPLES,
         }
         return render(request, self.template_name, content)
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:  # noqa: ANN002, ANN003
+        (
+            _,
+            _,
+        ) = args, kwargs
         form = TextBookForm(request.POST, request.FILES)
         if form.is_valid():
             book = form.save()
             try:
                 create_containers(book=book)
             except ContainerFileReadError as error:
-                error_text = f'Ошибка чтения файла: {error}'
+                error_text = f"Ошибка чтения файла: {error}"
                 form.add_error(None, error_text)
                 form.data._mutable = True
-                form.data['text'] = ''
+                form.data["text"] = ""
                 book.delete()
                 content = {
-                    'form': form,
+                    "form": form,
                 }
                 return render(request, self.template_name, content)
             else:
                 containers_count = book.containers.count()
-                messages.success(request, f'Книга создана, {containers_count} контейнеров')
-                return redirect('clients:book_list')
+                messages.success(request, f"Книга создана, {containers_count} контейнеров")
+                return redirect("clients:book_list")
         else:
             content = {
-                'form': form,
+                "form": form,
             }
             return render(request, self.template_name, content)
 
+
 @login_required
-def book_detail(request, book_id):
+def book_detail(request: HttpRequest, book_id: int) -> HttpResponse:
     book = Book.objects.get(pk=book_id)
     book_stat = get_book_stat(book=book)
     col_name = get_col_name_by_book(book=book)
@@ -72,41 +86,46 @@ def book_detail(request, book_id):
     containers_number_error = [container for container in containers if not iso6346.is_valid(container.number)]
     containers_past_30 = [container for container in containers if container.is_past_30()]
     content = {
-        'book': book,
-        'book_stat': book_stat,
-        'col_name': col_name,
-        'grouped_by_client': grouped_by_client,
-        'containers': containers,
-        'containers_no_area': containers_no_area,
-        'containers_number_error': containers_number_error,
-        'containers_past_30': containers_past_30,
+        "book": book,
+        "book_stat": book_stat,
+        "col_name": col_name,
+        "grouped_by_client": grouped_by_client,
+        "containers": containers,
+        "containers_no_area": containers_no_area,
+        "containers_number_error": containers_number_error,
+        "containers_past_30": containers_past_30,
     }
-    return render(request, 'clients/book/book_detail.html', content)
+    return render(request, "clients/book/book_detail.html", content)
+
 
 @login_required
-def book_no_containers_data(request, book_id):
+def book_no_containers_data(request: HttpRequest, book_id: int) -> HttpResponse:
+    _ = request
     book = Book.objects.get(pk=book_id)
-    text = book.no_containers_file.read().decode('utf-8')
-    return HttpResponse(f'<pre>{text}</pre>')
+    text = book.no_containers_file.read().decode("utf-8")
+    return HttpResponse(f"<pre>{text}</pre>")
+
 
 @login_required
-def book_file_original_text(request, book_id):
+def book_file_original_text(request: HttpRequest, book_id: int) -> HttpResponse:
+    _ = request
     book = Book.objects.get(pk=book_id)
-    text = book.file.read().decode('utf-8')
-    return HttpResponse(f'<pre>{text}</pre>')
+    text = book.file.read().decode("utf-8")
+    return HttpResponse(f"<pre>{text}</pre>")
 
 
 @login_required
-def book_delete(request, book_id):
+def book_delete(request: HttpRequest, book_id: int) -> HttpResponse:
     book = Book.objects.get(pk=book_id)
     book.delete()
-    messages.info(request, 'Книга удалена')
-    return redirect('clients:book_list')
+    messages.info(request, "Книга удалена")
+    return redirect("clients:book_list")
 
-def test(request):
+
+def test(request: HttpRequest) -> HttpResponse:
     content = {}
-    messages.success(request, 'success')
-    messages.error(request, 'error')
-    messages.warning(request, 'warning')
-    messages.info(request, 'info')
-    return render(request, 'clients/test.html', content)
+    messages.success(request, "success")
+    messages.error(request, "error")
+    messages.warning(request, "warning")
+    messages.info(request, "info")
+    return render(request, "clients/test.html", content)
